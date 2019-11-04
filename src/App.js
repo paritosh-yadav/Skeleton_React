927a1690de24a2fc26d70a915db1ff58634a0dd6
paritosh yadav <paritosh.yadav.237@gmail.com>
2019-11-04 14:21:01 +0530

    husky check

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 
-import React from "react";
+import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/Store";
@@ -11,25 +11,25 @@ import Profile from "./screens/Profile";
 import PrivateRoute from "./screens/PrivateRoute";
 import Settings from "./screens/settings/Settings.Container";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        {/* New - use BrowserRouter to provide access to /profile */}
-        <BrowserRouter>
-          <header>
-            <img src={logo} className="App-logo" alt="logo" />
-            <NavBar />
-          </header>
-          <Switch>
-            <Route path="/" exact />
-            <Route path="/settings" exact component={Settings} />
-            <PrivateRoute path="/profile" component={Profile} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    </Provider>
-  );
+function App()
+return (
+  <Provider store={store}>
+    <div className="App">
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/settings" exact component={Settings} />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  </Provider>
+);
 }
 
 export default App;
