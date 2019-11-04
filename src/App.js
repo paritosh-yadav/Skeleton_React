29809919f413d19b711ebe56e2d3550b25602e54
paritosh yadav <paritosh.yadav.237@gmail.com>
2019-11-04 14:22:18 +0530

    husky testing

--- src/App.js
+++ src/App.js
@@ -11,25 +11,25 @@ import Profile from "./screens/Profile";
 import PrivateRoute from "./screens/PrivateRoute";
 import Settings from "./screens/settings/Settings.Container";
 
-function App()
-return (
-  <Provider store={store}>
-    <div className="App">
-      {/* New - use BrowserRouter to provide access to /profile */}
-      <BrowserRouter>
-        <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <NavBar />
-        </header>
-        <Switch>
-          <Route path="/" exact />
-          <Route path="/settings" exact component={Settings} />
-          <PrivateRoute path="/profile" component={Profile} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  </Provider>
-);
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        {/* New - use BrowserRouter to provide access to /profile */}
+        <BrowserRouter>
+          <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <NavBar />
+          </header>
+          <Switch>
+            <Route path="/" exact />
+            <Route path="/settings" exact component={Settings} />
+            <PrivateRoute path="/profile" component={Profile} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
